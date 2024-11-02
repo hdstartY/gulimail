881c45c1d893f4391e2ceb97bcbe91b2e6d36a85
hdstarYu <haodongy86@gmail.com>
2024-11-02 16:35:51 +0800

    项目初始构建

---
+++ gulimail-coupon/src/main/java/com/hdstarYu/Main.java
@@ -0,0 +1,12 @@
+package com.hdstarYu;
+
+/**
+ * @author Dou li
+ * @creation data  2024-11-02-15:55
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
